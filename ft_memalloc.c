7b7c5720467c60426494c19db77311641e271a09
Michael Rivera <mirivera@e1z2r8p18.42.us.org>
2019-03-08 20:46:55 -0800

    revised Makefile and memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 09:33:26 by mirivera          #+#    #+#             */
-/*   Updated: 2019/02/28 09:10:25 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/03/08 20:39:33 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	*ft_memalloc(size_t size)
 {
-	void	*memory;
+	char	*memory;
 
-	if (!(memory = malloc(size)))
+	memory = malloc(size);
+	if (memory == NULL)
 		return (NULL);
 	ft_bzero(memory, size);
 	return (memory);
